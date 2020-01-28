e20a7d9d272fc73327d4d4cc4e29b7a34cd719b3
Mykhailo.Romaniuk <mykhailo.romaniuk@eleks.com>
2020-01-28 14:22:34 +0200

    add controller to manage site links

---
+++ src/ProjectHub.Api/Controllers/SiteLinksController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectHub.Data;
+using ProjectHub.Domain.Environment;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProjectHub.Api.Controllers
+{
+    [ApiController]
+    [Route("api/siteLinks")]
+    public class SiteLinksController : ControllerBase
+    {
+        private readonly HubContext _context;
+
+        public SiteLinksController(HubContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<SiteLink>> GetLinks()
+        {
+            return await _context.SiteLinks.ToListAsync();
+        }
+
+        [HttpGet("linkId")]
+        public async Task<SiteLink> GetLink(int linkId)
+        {
+            return await _context.SiteLinks.FindAsync(linkId);
+        }
+
+        [HttpPost("{linkId}")]
+        public async Task<IActionResult> UpdateLink(int linkId, [FromBody]SiteLink link)
+        {
+            if (linkId != link.Id)
+                return BadRequest();
+
+            _context.SiteLinks.Update(link);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
